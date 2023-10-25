abc202fc2d8e5aa008fcf79663a7f5d94dc08ce4
henryhuaman <henry.huamanc@usil.pe>
2023-10-24 23:35:12 -0500

    commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nombres = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        // a
+        List<String> cadAleatoria = Stream.generate(()->{
+            int longitud = generarNumeroAleatorio(0,9);
+            return Stream.generate(()-> {
+                        int index = generarNumeroAleatorio(0,nombres.length()-1);
+                        return nombres.charAt(index);
+                    }).limit(longitud)
+                    .map(Objects::toString)
+                    .collect(Collectors.joining());
+        }).limit(10)
+                .map(Object::toString)
+                .toList();
+
+        System.out.println(cadAleatoria);
+
+        //b.
+        int comp = (int) cadAleatoria.stream().
+                filter(word -> word.isEmpty())
+                .count();
+
+        System.out.println("numero de cadena que son nulas: "+comp);
+
+        //c.
+        int cadSupCinco = (int) cadAleatoria.stream().filter(word -> word.length()>5).count();
+
+        System.out.println("numero de cadena que tienen longitud mayor ae 5  : "+cadSupCinco);
+
+        //d.
+        int cadS = (int) cadAleatoria.stream().filter(word -> word.startsWith("s")).count();
+        System.out.println("numero de cademas que comienzan con S: "+cadS); 
+
+    }
+    public static int generarNumeroAleatorio(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("El valor mínimo debe ser menor que el valor máximo.");
+        }
+
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+}
